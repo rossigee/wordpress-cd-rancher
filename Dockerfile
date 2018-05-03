7800f034ed833a5d7777ae6cd92cb00f0d0f404c
Ross Golder <ross@golder.org>
2018-05-03 13:15:36 +0700

    Bump version to 0.1.1

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN apk -U add docker
 
 # Install the CI scripts
 ADD dist /dist
-RUN pip install /dist/wordpress-cd-rancher-0.1.0.tar.gz
+RUN pip install /dist/wordpress-cd-rancher-0.1.1.tar.gz
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'wordpress-cd-rancher',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Wordpress CD driver to deploy sites to Rancher environments.',
     author = 'Ross Golder',
     author_email = 'ross@golder.org',
